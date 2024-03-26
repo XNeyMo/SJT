d8def6386c48141b66e720c7fd445d8ccd465350
XNeyMo <xneymodev@gmail.com>
2024-03-26 16:35:01 -0500

    Again

---
+++ backend/target/debug/Jikan.d
@@ -0,0 +1,1 @@
+/home/xneymo/Documents/Codes/Jikan/target/debug/Jikan: /home/xneymo/Documents/Codes/Jikan/src/main.rs
---
+++ backend/target/debug/deps/Jikan-7b82768d0ff74cda.d
@@ -0,0 +1,5 @@
+/home/xneymo/Documents/Codes/Jikan/target/debug/deps/libJikan-7b82768d0ff74cda.rmeta: src/main.rs
+
+/home/xneymo/Documents/Codes/Jikan/target/debug/deps/Jikan-7b82768d0ff74cda.d: src/main.rs
+
+src/main.rs:
