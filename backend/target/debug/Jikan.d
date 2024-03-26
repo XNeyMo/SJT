7f1482ffce4bc031b8cdcfe0ef15bef83ca661ce
XNeyMo <xneymodev@gmail.com>
2024-03-26 16:37:39 -0500

    Error with .gitignore

--- backend/target/debug/Jikan.d
+++
@@ -1,1 +0,0 @@
-/home/xneymo/Documents/Codes/Jikan/target/debug/Jikan: /home/xneymo/Documents/Codes/Jikan/src/main.rs
--- backend/target/debug/deps/Jikan-df6e420a4d04521b.d
+++
@@ -1,5 +0,0 @@
-/home/xneymo/Documents/Codes/Jikan/target/debug/deps/libJikan-df6e420a4d04521b.rmeta: src/main.rs
-
-/home/xneymo/Documents/Codes/Jikan/target/debug/deps/Jikan-df6e420a4d04521b.d: src/main.rs
-
-src/main.rs:
