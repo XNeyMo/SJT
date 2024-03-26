37998ef4a870866f73f4f0e2b4b49b89bd2161e4
XNeyMo <xneymodev@gmail.com>
2024-03-26 16:34:24 -0500

    Error with .gitignore

--- backend/target/debug/deps/Jikan-2870ce3910a05d08.d
+++
@@ -1,5 +0,0 @@
-/home/xneymo/Documents/Codes/Jikan/target/debug/deps/Jikan-2870ce3910a05d08: src/main.rs
-
-/home/xneymo/Documents/Codes/Jikan/target/debug/deps/Jikan-2870ce3910a05d08.d: src/main.rs
-
-src/main.rs:
