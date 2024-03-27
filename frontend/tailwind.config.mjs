351f76bc4bd7069e432ab7396405700e1b8d474a
XNeyMo <xneymodev@gmail.com>
2024-03-26 23:01:51 -0500

    Landing Page Initialized

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			'black': '#263238',
+			'white': '#ffffff',
+			'amethyst': {
+				'50': '#faf7fc',
+				'100': '#f5eef9',
+				'200': '#ebdcf2',
+				'300': '#dcc1e6',
+				'400': '#c69bd7',
+				'500': '#a05fb9',
+				'600': '#9054a5',
+				'700': '#784388',
+				'800': '#63396f',
+				'900': '#54325d',
+				'950': '#33183a',
+			},
+		},
 	},
 	plugins: [],
 }
