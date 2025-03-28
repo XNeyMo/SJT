11f04316680f1cf0c21cea1699caeb54b149b4c9
XNeyMo <xneymodev@protonmail.com>
2025-03-28 02:36:09 -0500

    no

--- frontend/app/(default)/page.tsx
+++ frontend/app/(default)/page.tsx
@@ -1,13 +1,18 @@
 import Footer from "@components/Footer";
 import DefaultHeader from "@components/header/DefaultHeader";
+import ScheduleForm from "@components/ScheduleForm";
 
 export default function Home() {
   return (
     <main>
       <DefaultHeader />
 
-      <section className="h-screen">
+      <section className="h-screen p-20">
+        <h1 className="text-2xl font-bold mb-4">Schedule Planner</h1>
 
+        <div className="h-full">
+          <ScheduleForm /> {/* Agrega el formulario de horario aquí */}
+        </div>
       </section>
 
       <Footer />
