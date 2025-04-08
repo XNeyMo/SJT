eaef2278834f1233608269ff2ed0c5696c76a2e9
XNeyMo <xneymodev@protonmail.com>
2025-04-08 02:06:11 -0500

    se me olvidó la ruta

--- frontend/hooks/config.ts
+++ frontend/hooks/config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:9090',
+  baseURL: 'https://saijitan.onrender.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
