b06031a85b532437ece3cfe15543149230b86cbe
XNeyMo <xneymodev@protonmail.com>
2025-04-08 21:47:30 -0500

    i forgot something

--- frontend/hooks/config.ts
+++ frontend/hooks/config.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  // baseURL: 'https://saijitan.onrender.com',
-  baseURL: 'http://localhost:9090',
+  baseURL: 'https://saijitan.onrender.com',
+  // baseURL: 'http://localhost:9090',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
